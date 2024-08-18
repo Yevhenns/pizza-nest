858ea96e8ce30eaf876dc55523eae10a1293ca8d
Yevhenns <gifestum@gmail.com>
2024-08-18 17:02:28 +0300

    connected db

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './schemas/product.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
+
+  async findAll(): Promise<Product[]> {
+    return this.productModel.find().exec();
+  }
+}
