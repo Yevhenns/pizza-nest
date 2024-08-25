140024666174e00fb5777d799668d8ba2864a212
Yevhenns <gifestum@gmail.com>
2024-08-25 13:59:34 +0300

    added ApiProperty for schema

--- src/send-email/dto/createEmailBody.dto.ts
+++ src/send-email/dto/createEmailBody.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class createEmailBodyDto {
+  @ApiProperty({
+    example: {
+      address: 'Лесі Українки 12/32',
+      comment: 'На 18:30',
+      delivery: true,
+      name: 'Андрій',
+      number: '0938882211',
+    },
+  })
   customerInfo: {
     address: string;
     comment: string;
@@ -6,10 +17,18 @@ export class createEmailBodyDto {
     name: string;
     number: string;
   };
+  @ApiProperty({
+    example: {
+      optionsTitles: ['Сирний бортик'],
+      quantity: 2,
+      title: 'Карбонара',
+    },
+  })
   order: {
     optionsTitles: string[];
     quantity: number;
     title: string;
   };
+  @ApiProperty({ example: 250 })
   orderSum: number;
 }
