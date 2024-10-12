0650244ad78250677e761e42d62fd76b9eb168c3
Yevhenns <gifestum@gmail.com>
2024-10-12 18:55:44 +0300

    deleted slash

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://nostrra-pizzza.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://nostrra-pizzza.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
