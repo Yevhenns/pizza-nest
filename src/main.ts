d73b89fbc5fac2dda3a278469b2ee25c51d22d52
Yevhenns <gifestum@gmail.com>
2025-01-30 18:46:23 +0200

    added delete method

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:3000', 'https://nostrra-pizzza.vercel.app'],
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
   });
 
